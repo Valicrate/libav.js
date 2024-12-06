bc40eac19b8c9e954593371b92ea5c2093dc4f69
daniellovera <162410607+daniellovera@users.noreply.github.com>
2024-12-06 22:48:49 +0000

    added cli to sink

--- configs/configs/sink/deps.mk
+++ configs/configs/sink/deps.mk
@@ -2,3 +2,5 @@ build/ffmpeg-$(FFMPEG_VERSION)/build-base-sink/ffbuild/config.mak: build/inst/ba
 build/ffmpeg-$(FFMPEG_VERSION)/build-thr-sink/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/opus.pc
 build/ffmpeg-$(FFMPEG_VERSION)/build-base-sink/ffbuild/config.mak: build/inst/base/lib/pkgconfig/vorbis.pc
 build/ffmpeg-$(FFMPEG_VERSION)/build-thr-sink/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/vorbis.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-base-sink/ffbuild/config.mak: build/inst/base/lib/libmp3lame.a
+build/ffmpeg-$(FFMPEG_VERSION)/build-thr-sink/ffbuild/config.mak: build/inst/thr/lib/libmp3lame.a
--- configs/mkconfigs.js
+++ configs/mkconfigs.js
@@ -38,13 +38,118 @@ const formats = ["format-mp4", "format-webm", "format-matroska", "format-avi"];
 // demuxer-3gp not need, included in -mov | demuxer-mpeg doesn't match anything, changed to demuxer-mpegps, nor does demuxer-ipod (it's only a muxer for m4a)
 // removed demuxer-dash as we also need to include libxml2 and we aren't going to receive any streams over http to demux
 // Demuxers list
-const dmux = ["demuxer-asf","demuxer-flv","demuxer-hls","demuxer-mov","demuxer-mp4","demuxer-mpegts", "demuxer-mpegps", "demuxer-rawvideo","demuxer-ogg","demuxer-matroska", "demuxer-avi", "demuxer-webm","demuxer-dhav", "demuxer-dv", "demuxer-m4v", "parser-mpegvideo"];
+const dmux = [
+  "demuxer-asf",
+  "demuxer-flv",
+  "demuxer-hls",
+  "demuxer-mov",
+  "demuxer-mp4",
+  "demuxer-mpegts",
+  "demuxer-mpegps",
+  "demuxer-rawvideo",
+  "demuxer-ogg",
+  "demuxer-matroska",
+  "demuxer-avi",
+  "demuxer-webm",
+  "demuxer-dhav",
+  "demuxer-dv",
+  "demuxer-m4v",
+  "parser-mpegvideo",
+];
 
 // Video Decoders and parsers list
-const vdecpar = ["parser-vp8","parser-vp9","parser-av1","parser-h264","parser-hevc","bsf-extract_extradata","bsf-vp9_metadata","bsf-av1_metadata","bsf-h264_metadata","bsf-hevc_metadata", "decoder-cinepak", "decoder-dvvideo", "decoder-flv","decoder-h261","parser-h261", "decoder-h263", "parser-h263", "decoder-h263i", "decoder-h263p", "decoder-mpeg1video", "decoder-mpeg2video","bsf-mpeg2_metadata", "decoder-mpegvideo","parser-mpegvideo", "decoder-mpeg4","bsf-mpeg4_unpack_bframes", "parser-mpeg4video", "decoder-msmpeg4v1","decoder-msmpeg4v2","decoder-msmpeg4v3", "decoder-msvideo1", "decoder-prores", "decoder-rawvideo", "decoder-theora", "decoder-vp6", "parser-vc1","parser-vp3", "decoder-wmv1", "decoder-wmv2", "decoder-wmv3"];
+const vdecpar = [
+  "parser-vp8",
+  "parser-vp9",
+  "parser-av1",
+  "parser-h264",
+  "parser-hevc",
+  "bsf-extract_extradata",
+  "bsf-vp9_metadata",
+  "bsf-av1_metadata",
+  "bsf-h264_metadata",
+  "bsf-hevc_metadata",
+  "decoder-cinepak",
+  "decoder-dvvideo",
+  "decoder-flv",
+  "decoder-h261",
+  "parser-h261",
+  "decoder-h263",
+  "parser-h263",
+  "decoder-h263i",
+  "decoder-h263p",
+  "decoder-mpeg1video",
+  "decoder-mpeg2video",
+  "bsf-mpeg2_metadata",
+  "decoder-mpegvideo",
+  "parser-mpegvideo",
+  "decoder-mpeg4",
+  "bsf-mpeg4_unpack_bframes",
+  "parser-mpeg4video",
+  "decoder-msmpeg4v1",
+  "decoder-msmpeg4v2",
+  "decoder-msmpeg4v3",
+  "decoder-msvideo1",
+  "decoder-prores",
+  "decoder-rawvideo",
+  "decoder-theora",
+  "decoder-vp6",
+  "parser-vc1",
+  "parser-vp3",
+  "decoder-wmv1",
+  "decoder-wmv2",
+  "decoder-wmv3",
+];
 
 // Audio Decoders and parsers list
-const adecpar = ["parser-ftr","parser-adx","parser-mlp","format-aac","parser-aac", "parser-aac_latm", "codec-aac", "parser-opus", "codec-libopus", "format-flac", "parser-flac", "codec-flac", "format-mp3","decoder-mp3", "format-pcm_f32le", "codec-pcm_f32le","codec-prores", "codec-qtrle", "format-wav", "codec-libvorbis", "codec-alac", "decoder-ac3", "parser-ac3","decoder-dolby_e","parser-dolby_e", "decoder-eac3","bsf-eac3_core", "decoder-dvaudio","parser-dvaudio","decoder-dca", "parser-dca","bsf-dca_core","bsf-dovi_rpu","bsf-truehd_core", "decoder-mp1","parser-mpegaudio", "decoder-mp2", "decoder-pcm_dvd", "decoder-pcm_bluray", "decoder-pcm_s16le", "decoder-pcm_s24le", "decoder-wmav1","decoder-wmav2","decoder-wmapro","decoder-wmavoice","parser-xma",];
+const adecpar = [
+  "parser-ftr",
+  "parser-adx",
+  "parser-mlp",
+  "format-aac",
+  "parser-aac",
+  "parser-aac_latm",
+  "codec-aac",
+  "parser-opus",
+  "codec-libopus",
+  "format-flac",
+  "parser-flac",
+  "codec-flac",
+  "format-mp3",
+  "decoder-mp3",
+  "format-pcm_f32le",
+  "codec-pcm_f32le",
+  "codec-prores",
+  "codec-qtrle",
+  "format-wav",
+  "codec-libvorbis",
+  "codec-alac",
+  "decoder-ac3",
+  "parser-ac3",
+  "decoder-dolby_e",
+  "parser-dolby_e",
+  "decoder-eac3",
+  "bsf-eac3_core",
+  "decoder-dvaudio",
+  "parser-dvaudio",
+  "decoder-dca",
+  "parser-dca",
+  "bsf-dca_core",
+  "bsf-dovi_rpu",
+  "bsf-truehd_core",
+  "decoder-mp1",
+  "parser-mpegaudio",
+  "decoder-mp2",
+  "decoder-pcm_dvd",
+  "decoder-pcm_bluray",
+  "decoder-pcm_s16le",
+  "decoder-pcm_s24le",
+  "decoder-wmav1",
+  "decoder-wmav2",
+  "decoder-wmapro",
+  "decoder-wmavoice",
+  "parser-xma",
+];
 
 // Video encoders list
 // removed "encoder-h263" and "encoder-h263p", both in favor of "codec-mpeg4" to encode H.263 with mpeg4 in the form of DivX
@@ -52,198 +157,274 @@ const adecpar = ["parser-ftr","parser-adx","parser-mlp","format-aac","parser-aac
 const videnc = ["codec-mpeg4"];
 
 // Audio encoders list
-//const audenc = ["codec-anull"];
+//removed "codec-anull"
+const audenc = ["encoder-libmp3lame"];
 
-// Muxers list
+// Muxers lists
 const mux = ["muxer-ipod"];
 
 // Filters list
 const avfilt = ["audio-filters", "video-filters", "protocol-jsfetch"];
 
 const configsRaw = [
-    // Audio sensible:
-    ["default", [
-        "format-ogg", "format-webm",
-        opus, flac, "format-wav", "codec-pcm_f32le",
-        "audio-filters"
-    ], {cli: true}],
+  // Audio sensible:
+  [
+    "default",
+    [
+      "format-ogg",
+      "format-webm",
+      opus,
+      flac,
+      "format-wav",
+      "codec-pcm_f32le",
+      "audio-filters",
+    ],
+    { cli: true },
+  ],
 
-    ["opus", ["format-ogg", "format-webm", opus], {af: true}],
-    ["flac", ["format-ogg", flac], {af: true}],
-    ["wav", ["format-wav", "codec-pcm_f32le"], {af: true}],
+  ["opus", ["format-ogg", "format-webm", opus], { af: true }],
+  ["flac", ["format-ogg", flac], { af: true }],
+  ["wav", ["format-wav", "codec-pcm_f32le"], { af: true }],
 
-    // Audio silly:
-    ["obsolete", [
-        // Modern:
-        "format-ogg", "format-webm",
-        opus, flac,
+  // Audio silly:
+  [
+    "obsolete",
+    [
+      // Modern:
+      "format-ogg",
+      "format-webm",
+      opus,
+      flac,
 
-        // Timeless:
-        "format-wav", "codec-pcm_f32le",
+      // Timeless:
+      "format-wav",
+      "codec-pcm_f32le",
 
-        // Obsolete:
-        "codec-libvorbis", mp3,
+      // Obsolete:
+      "codec-libvorbis",
+      mp3,
 
-        // (and filters)
-        "audio-filters"
-    ]],
+      // (and filters)
+      "audio-filters",
+    ],
+  ],
 
-    // Audio reprobate:
-    ["aac", ["format-mp4", "format-aac", "format-webm", aac], {af: true}],
+  // Audio reprobate:
+  ["aac", ["format-mp4", "format-aac", "format-webm", aac], { af: true }],
 
-    // Video sensible:
-    ["webm", [
-        "format-ogg", "format-webm",
-        opus, flac, "format-wav", "codec-pcm_f32le",
-        "audio-filters",
+  // Video sensible:
+  [
+    "webm",
+    [
+      "format-ogg",
+      "format-webm",
+      opus,
+      flac,
+      "format-wav",
+      "codec-pcm_f32le",
+      "audio-filters",
 
-        "libvpx", vp8,
-        "swscale", "video-filters"
-    ], {vp9: true, cli: true}],
+      "libvpx",
+      vp8,
+      "swscale",
+      "video-filters",
+    ],
+    { vp9: true, cli: true },
+  ],
 
-    ["vp8-opus", ["format-ogg", "format-webm", opus, "libvpx", vp8], {avf: true}],
-    ["vp9-opus", ["format-ogg", "format-webm", opus, "libvpx", vp9], {avf: true}],
-    ["av1-opus", ["format-ogg", "format-webm", opus, aomav1], {avf: true}],
+  [
+    "vp8-opus",
+    ["format-ogg", "format-webm", opus, "libvpx", vp8],
+    { avf: true },
+  ],
+  [
+    "vp9-opus",
+    ["format-ogg", "format-webm", opus, "libvpx", vp9],
+    { avf: true },
+  ],
+  ["av1-opus", ["format-ogg", "format-webm", opus, aomav1], { avf: true }],
 
-    // Video reprobate:
-    ["h264-aac", ["format-mp4", "format-aac", "format-webm", aac, h264], {avf: true}],
-    ["hevc-aac", ["format-mp4", "format-aac", "format-webm", aac, hevc], {avf: true}],
+  // Video reprobate:
+  [
+    "h264-aac",
+    ["format-mp4", "format-aac", "format-webm", aac, h264],
+    { avf: true },
+  ],
+  [
+    "hevc-aac",
+    ["format-mp4", "format-aac", "format-webm", aac, hevc],
+    { avf: true },
+  ],
 
-    // Mostly parsing:
-    ["webcodecs", [
-        "format-ogg", "format-webm", "format-mp4",
-        opus, flac, "format-wav", "codec-pcm_f32le",
-        "parser-aac",
+  // Mostly parsing:
+  [
+    "webcodecs",
+    [
+      "format-ogg",
+      "format-webm",
+      "format-mp4",
+      opus,
+      flac,
+      "format-wav",
+      "codec-pcm_f32le",
+      "parser-aac",
 
-        "parser-vp8", "parser-vp9", "parser-av1",
-        "parser-h264", "parser-hevc",
+      "parser-vp8",
+      "parser-vp9",
+      "parser-av1",
+      "parser-h264",
+      "parser-hevc",
 
-        "bsf-extract_extradata",
-        "bsf-vp9_metadata", "bsf-av1_metadata",
-        "bsf-h264_metadata", "bsf-hevc_metadata"
-    ], {avf: true}],
+      "bsf-extract_extradata",
+      "bsf-vp9_metadata",
+      "bsf-av1_metadata",
+      "bsf-h264_metadata",
+      "bsf-hevc_metadata",
+    ],
+    { avf: true },
+  ],
 
-    // These are here so that "all" will have them for testing
-    ["extras", [
-        // Images
-        "format-image2", "demuxer-image_gif_pipe", "demuxer-image_jpeg_pipe",
-        "demuxer-image_png_pipe",
-        "parser-gif", "codec-gif", "parser-mjpeg",
-        "codec-mjpeg", "parser-png", "codec-png", "parser-webp", "decoder-webp",
+  // These are here so that "all" will have them for testing
+  [
+    "extras",
+    [
+      // Images
+      "format-image2",
+      "demuxer-image_gif_pipe",
+      "demuxer-image_jpeg_pipe",
+      "demuxer-image_png_pipe",
+      "parser-gif",
+      "codec-gif",
+      "parser-mjpeg",
+      "codec-mjpeg",
+      "parser-png",
+      "codec-png",
+      "parser-webp",
+      "decoder-webp",
 
-        // Raw data
-        "format-rawvideo", "codec-rawvideo",
-        "format-pcm_f32le", "codec-pcm_f32le",
+      // Raw data
+      "format-rawvideo",
+      "codec-rawvideo",
+      "format-pcm_f32le",
+      "codec-pcm_f32le",
 
-        // Apple-flavored lossless
-        "codec-alac", "codec-prores", "codec-qtrle",
+      // Apple-flavored lossless
+      "codec-alac",
+      "codec-prores",
+      "codec-qtrle",
 
-        // HLS
-        "format-hls", "protocol-jsfetch"
-    ]],
+      // HLS
+      "format-hls",
+      "protocol-jsfetch",
+    ],
+  ],
 
-    // The set of options for Sink
-    ["sink", [
-        "swscale",
-        formats, dmux, mux, vdecpar, adecpar, videnc, avfilt
-    ]],
+  // The set of options for Sink
+  [
+    "sink",
+    [
+      "swscale",
+      formats,
+      dmux,
+      mux,
+      vdecpar,
+      adecpar,
+      videnc,
+      audenc,
+      avfilt,
+      "cli",
+    ],
+  ],
 
-    ["empty", []],
-    ["all", null]
+  ["empty", []],
+  ["all", null],
 ];
 let all = Object.create(null);
 
 function configGroup(configs, nameExt, parts) {
-    const toAdd = configs.map(config =>
-        [`${config[0]}-${nameExt}`, config[1].concat(parts)]
-    );
-    configs.push.apply(configs, toAdd);
+  const toAdd = configs.map((config) => [
+    `${config[0]}-${nameExt}`,
+    config[1].concat(parts),
+  ]);
+  configs.push.apply(configs, toAdd);
 }
 
 // Process the configs into groups
 const configs = [];
 for (const config of configsRaw) {
-    const [name, inParts, extra] = config;
+  const [name, inParts, extra] = config;
 
-    // Expand the parts
-    const parts = inParts ? [] : null;
-    if (inParts) {
-        for (const part of inParts) {
-            if (part instanceof Array)
-                parts.push.apply(parts, part);
-            else
-                parts.push(part);
-        }
+  // Expand the parts
+  const parts = inParts ? [] : null;
+  if (inParts) {
+    for (const part of inParts) {
+      if (part instanceof Array) parts.push.apply(parts, part);
+      else parts.push(part);
     }
+  }
 
-    // Expand the extras
-    const toAdd = [[name, parts]];
-    if (extra && extra.vp9)
-        configGroup(toAdd, "vp9", vp9);
-    if (extra && extra.af)
-        configGroup(toAdd, "af", ["audio-filters"]);
-    if (extra && extra.avf)
-        configGroup(toAdd, "avf", ["audio-filters", "swscale", "video-filters"]);
-    if (extra && extra.cli)
-        configGroup(toAdd, "cli", ["cli"]);
+  // Expand the extras
+  const toAdd = [[name, parts]];
+  if (extra && extra.vp9) configGroup(toAdd, "vp9", vp9);
+  if (extra && extra.af) configGroup(toAdd, "af", ["audio-filters"]);
+  if (extra && extra.avf)
+    configGroup(toAdd, "avf", ["audio-filters", "swscale", "video-filters"]);
+  if (extra && extra.cli) configGroup(toAdd, "cli", ["cli"]);
 
-    configs.push.apply(configs, toAdd);
+  configs.push.apply(configs, toAdd);
 }
 
 // Process arguments
 let createOnes = false;
 for (const arg of process.argv.slice(2)) {
-    if (arg === "--create-ones")
-        createOnes = true;
-    else {
-        console.error(`Unrecognized argument ${arg}`);
-        process.exit(1);
-    }
+  if (arg === "--create-ones") createOnes = true;
+  else {
+    console.error(`Unrecognized argument ${arg}`);
+    process.exit(1);
+  }
 }
 
 async function main() {
-    for (let [name, config] of configs) {
-        if (name !== "all") {
-            for (const fragment of config)
-                all[fragment] = true;
-        } else {
-            config = Object.keys(all);
-        }
-
-        const p = cproc.spawn("./mkconfig.js", [name, JSON.stringify(config)], {
-            stdio: "inherit"
-        });
-        await new Promise(res => p.on("close", res));
+  for (let [name, config] of configs) {
+    if (name !== "all") {
+      for (const fragment of config) all[fragment] = true;
+    } else {
+      config = Object.keys(all);
     }
 
-    if (createOnes) {
-        const allFragments = Object.keys(all).map(x => {
-            // Split up codecs and formats
-            const p = /^([^-]*)-(.*)$/.exec(x);
-            if (!p)
-                return [x];
-            if (p[1] === "codec")
-                return [`decoder-${p[2]}`, `encoder-${p[2]}`, x]
-            else if (p[1] === "format")
-                return [`demuxer-${p[2]}`, `muxer-${p[2]}`, x]
-            else
-                return [x];
-        }).reduce((a, b) => a.concat(b));
+    const p = cproc.spawn("./mkconfig.js", [name, JSON.stringify(config)], {
+      stdio: "inherit",
+    });
+    await new Promise((res) => p.on("close", res));
+  }
 
-        for (const fragment of allFragments) {
-            // Fix fragment dependencies
-            let fragments = [fragment];
-            if (fragment.indexOf("libvpx") >= 0)
-                fragments.unshift("libvpx");
-            if (fragment === "parser-aac")
-                fragments.push("parser-ac3");
+  if (createOnes) {
+    const allFragments = Object.keys(all)
+      .map((x) => {
+        // Split up codecs and formats
+        const p = /^([^-]*)-(.*)$/.exec(x);
+        if (!p) return [x];
+        if (p[1] === "codec") return [`decoder-${p[2]}`, `encoder-${p[2]}`, x];
+        else if (p[1] === "format")
+          return [`demuxer-${p[2]}`, `muxer-${p[2]}`, x];
+        else return [x];
+      })
+      .reduce((a, b) => a.concat(b));
 
-            // And make the variant
-            const p = cproc.spawn("./mkconfig.js", [
-                `one-${fragment}`, JSON.stringify(fragments)
-            ], {stdio: "inherit"});
-            await new Promise(res => p.on("close", res));
-        }
+    for (const fragment of allFragments) {
+      // Fix fragment dependencies
+      let fragments = [fragment];
+      if (fragment.indexOf("libvpx") >= 0) fragments.unshift("libvpx");
+      if (fragment === "parser-aac") fragments.push("parser-ac3");
+
+      // And make the variant
+      const p = cproc.spawn(
+        "./mkconfig.js",
+        [`one-${fragment}`, JSON.stringify(fragments)],
+        { stdio: "inherit" }
+      );
+      await new Promise((res) => p.on("close", res));
     }
+  }
 }
 main();
